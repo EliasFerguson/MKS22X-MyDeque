3cd176f21992eb954d55a6ec17e5e7400a76cc4f
Elias Ferguson <eferguson00@stuy.edu>
2019-03-21 11:28:01 -0400

    bogus return values

--- MyDeque.java
+++ MyDeque.java
@@ -4,12 +4,24 @@ public class MyDeque<E>{
 
   public MyDeque(){  }
   public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
+  public int size(){
+    return 0;
+  }
+  public String toString(){
+    return "";
+  }
   public void addFirst(E element){ }
   public void addLast(E element){ }
-  public E removeFirst(){ }
-  public E removeLast(){ }
-  public E getFirst(){ }
-  public E getLast(){ }
+  public E removeFirst(){
+    return null;
+  }
+  public E removeLast(){
+    return null;
+  }
+  public E getFirst(){
+    return null;
+  }
+  public E getLast(){
+    return null;
+  }
 }
