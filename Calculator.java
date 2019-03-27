744f569ecbbb006e2fbda8e36ad7cbf7776ea955
Elias Ferguson <eferguson00@stuy.edu>
2019-03-27 14:48:03 -0400

    working through eval method

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class Calculator {
   public static double eval(String s) {
+    String[] tokens = s.split(" ");
+    MyDeque<String> deque = new MyDeque<String>();
+    for (String token: tokens) {
+      if (!isOperation(token)) deque.addLast(token);
+      else {
+        Double a = Double.parseDouble(deque.removeLast());
+        Double b = Double.parseDouble(deque.removeLast());
+      }
+    }
     return 1 + 1.0;
   }
   public static boolean isOperation(String input) {
